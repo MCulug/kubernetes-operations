34ab0912744c2187b4b5814a6745d30e5c327caa
Melih Can Ulug <melihcanulug@hotmail.com>
2022-08-01 08:59:20 +0300

    Update delete all finalizers.py

--- delete all finalizers.py
+++ delete all finalizers.py
@@ -1,1 +1,10 @@
-// delete all finalizers
+#delete all finalizers
+
+ResourceType = input("Enter the type of the resource that you want to delete: ")
+
+#check if Resource Type is valid
+
+def Check_Kube_Resource_Type():
+    Kube_Resource = ["svc","rs","ss","","pod", "Deployment", "StatefulSet","Secret","ConfigMap","Job","DaemonSet","ReplicaSet","ReplicationController","Service","Route","Ingress","NetworkPolicy","Project","CustomResourceDefinition","ClusterServiceVersion","Operator","Subscription","ClusterVersion","namespace"]
+	if ResourceType =
+	statement(s)
